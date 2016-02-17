95f1c41fe5994f5ba03385db7ac683b268ae3e88
James <jgstew101@gmail.com>
2016-02-17 13:40:15 -0600

    finished part 2

--- osp2b.cpp
+++ osp2b.cpp
@@ -3,26 +3,28 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 using namespace std;
 
 //Functions
 void *printString(void *ptr);
-bool isConsonant(string x);
-bool isVowel(string x);
-bool isOther(string x);
+bool isConsonant(char x);
+bool isVowel(char x);
+bool isOther(char x);
 
 //Global variables
 string token;
-const char** input;
-int totalInput;
+const char** gargv;
+int gargc;
+int gindex;
 
 int main(int argc, char const *argv[]) {
 
   pthread_t cThread, vThread, oThread;
-  char firstChar;
-  input = argv;
-  totalInput = argc;
+  gargv = argv;
+  gargc = argc;
+  gindex = 1;
 
   int idConsonant = 0;
   int idVowel = 1;
@@ -42,77 +44,55 @@ int main(int argc, char const *argv[]) {
 void *printString(void *ptr) {
   int ii;
   string token;
-  string firstChar;
+  char firstChar;
   int id = *((int *) ptr);
-  printf("%d\n", id);
+  int allowed = 0;
+  bool changeThread = false;
 
-  for (ii = 1; ii < totalInput; ii++) {
 
-    token = input[ii];
+  while (gindex < gargc) {
+    token = gargv[gindex];
     firstChar = token.at(0);
 
-    while(true) {
-      if (isConsonant(firstChar) && id == 0) {
-        cout << token << endl;
-        sched_yield();
-        break;
-      }
-      else if(isVowel(firstChar) && id == 1) {
-        cout << token << endl;
-        sched_yield();
-        break;
-      }
-      else if (isOther(firstChar) && id == 2) {
-        cout << token << endl;
-        sched_yield();
-        break;
-      }
+    //lines that are commented out will print the thread id and token
+    if (isConsonant(firstChar) && id == 0) {
+      // cout << id << " " << token << endl;
+      cout << token << endl;
+      gindex++;
+    } else if (isVowel(firstChar) && id == 1) {
+      // cout << id << " " << token <<endl;
+      cout << token << endl;
+      gindex++;
+    } else if(isOther(firstChar) && id == 2) {
+      // cout << id << " " << token << endl;
+      cout << token << endl;
+      gindex++;
+    } else {
+      //sched_yield();
     }
   }
   pthread_exit(0);
 }
 
-bool isConsonant(string x) {
-  string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
-  int numberOfConsonants = 42;
-  string consonant;
-  int ii;
-
-  for(ii = 0; ii < numberOfConsonants; ii++) {
-    consonant = consonants.at(ii);
-    if (x.compare(consonant) == 0) {
-      return true;
-    }
+bool isConsonant(char x) {
+  char consonants[] = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+  if (strchr(consonants, x) != NULL) {
+    return true;
   }
   return false;
 }
 
-bool isVowel(string x) {
-  string vowels = "aeiouAEIOU";
-  int numberOfVowels = 10;
-  string vowel;
-  int ii;
-
-  for(ii = 0; ii < numberOfVowels; ii++) {
-    vowel = vowels.at(ii);
-    if(x.compare(vowel) == 0) {
-      return true;
-    }
+bool isVowel(char x) {
+  char vowels[] = "aeiouAEIOU";
+  if (strchr(vowels, x) != NULL) {
+    return true;
   }
   return false;
 }
 
-bool isOther(string x) {
-  string others = "0123456789.,!@#$^&*()[]{};:/?<>`~|";
-  int numberOfOthers = 34;
-  string other;
-  int ii;
-
-  for(ii = 0; ii < numberOfOthers; ii++) {
-    other = others.at(ii);
-    if(x.compare(other) == 0) {
-      return true;
-    }
+bool isOther(char x) {
+  if (!isalpha(x)) {
+    return true;
   }
   return false;
 }
