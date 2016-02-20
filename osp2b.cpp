b97263f0795e4c2ff8d21e1be098a0b83009ae2d
James <jgstew101@gmail.com>
2016-02-19 21:37:11 -0600

    commented and ready to go

--- osp2b.cpp
+++ osp2b.cpp
@@ -1,3 +1,25 @@
+//*********************************************************
+//
+// James Stewart and Will Taylor
+// Operating Systems
+// Project #2b
+//
+// Due: Monday, Feb. 22 at Midnight
+//
+// This multithreaded program uses PThreads to print out
+// arguments entered by the user upon execution. Each of the
+// three threads will take turns printing out strings that
+// begin with a consonant, vowel, or other.
+//
+//  Input: A series of n strings entered as arguments to the
+//  executable.
+//
+//  Output: The arguments entered by the user preceded with
+//  what kind of character it starts with, where each argument
+//  is printed on its own line.
+//
+//*********************************************************
+
 #include <pthread.h>
 #include <string>
 #include <iostream>
@@ -17,29 +39,59 @@ bool isOther(char x);
 string token;
 const char** gargv;
 int gargc;
-int gindex;
+int gindex; //argument counter
+
+/*****************************************************************************
+    main() function
+
+    Purpose - Assigns the values entered by the user to global variables and
+              creates the threads that are then assigned to printString().
+
+    Input - int as argc; const char array as argv.
+
+    Output - None.
+
+*****************************************************************************/
 
 int main(int argc, char const *argv[]) {
 
   pthread_t cThread, vThread, oThread;
   gargv = argv;
   gargc = argc;
-  gindex = 1;
+  gindex = 1; //start at 1 to skip executable
 
+  //ids for the threads
   int idConsonant = 0;
   int idVowel = 1;
   int idOther = 2;
 
+  //create the threads
   int iretC = pthread_create(&cThread, NULL, *printString, (void*) &idConsonant);
   int iretV = pthread_create(&vThread, NULL, *printString, (void*) &idVowel);
   int iretO = pthread_create(&oThread, NULL, *printString, (void*) &idOther);
 
+  //make them wait for each other before exiting out
   pthread_join(cThread, NULL);
   pthread_join(vThread, NULL);
   pthread_join(oThread, NULL);
 
   return 0;
 }
+
+/****************************************************************************
+*   printString() function
+*
+*   Purpose - Threads wait in a loop until it is their turn to print the
+*              string that corresponds to the type of character they are
+*              responsible for printing.
+*
+*    Input - Threads
+*
+*    Output - Strings printed out to the standard output that show what type
+*             the first character is and the string the character belonged to.
+*
+*
+****************************************************************************/
 
 void *printString(void *ptr) {
   int ii;
@@ -49,48 +101,86 @@ void *printString(void *ptr) {
   int allowed = 0;
   bool changeThread = false;
 
-
+  //while there is still another argument
   while (gindex < gargc) {
     token = gargv[gindex];
     firstChar = token.at(0);
 
-    //lines that are commented out will print the thread id and token
     if (isConsonant(firstChar) && id == 0) {
-      // cout << id << " " << token << endl;
-      cout << token << endl;
+      cout << "CONSONANT: " << token << endl;
       gindex++;
     } else if (isVowel(firstChar) && id == 1) {
-      // cout << id << " " << token <<endl;
-      cout << token << endl;
+      cout << "VOWEL: " << token << endl;
       gindex++;
     } else if(isOther(firstChar) && id == 2) {
-      // cout << id << " " << token << endl;
-      cout << token << endl;
+      cout << "OTHER: " << token << endl;
       gindex++;
     } else {
-      //sched_yield();
+      sched_yield();
     }
   }
   pthread_exit(0);
 }
 
+/**********************************************************************
+*    isConsonant() function
+*
+*    Purpose - This function will return true if the character it is passed
+*              begins with a consonant; otherwise it returns false.
+*
+*    Input - This function takes a character.
+*
+*    Output - Boolean.
+*
+**********************************************************************/
+
 bool isConsonant(char x) {
   char consonants[] = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+
+  // does the array contain x?
   if (strchr(consonants, x) != NULL) {
     return true;
   }
   return false;
 }
 
+/**********************************************************************
+*    isVowel() function
+*
+*    Purpose - This function will return true if the character it is passed
+*              begins with a vowel; otherwise it returns false.
+*
+*    Input - This function takes a character.
+*
+*    Output - Boolean.
+*
+**********************************************************************/
+
 bool isVowel(char x) {
   char vowels[] = "aeiouAEIOU";
+
+  // does the array contain x?
   if (strchr(vowels, x) != NULL) {
     return true;
   }
   return false;
 }
 
+/**********************************************************************
+*    isOther() function
+*
+*    Purpose - This function will return true if the character it is passed
+*              begins with a non-alpha character; otherwise it returns false.
+*
+*    Input - This function takes a character.
+*
+*    Output - Boolean.
+*
+**********************************************************************/
+
 bool isOther(char x) {
+
+  //is x an alpha character?
   if (!isalpha(x)) {
     return true;
   }
